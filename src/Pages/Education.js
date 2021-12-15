1811a99b0c0eca937b3f2997a91e62888cf312a9
orgil0588 <76672549+orgil0588@users.noreply.github.com>
2021-12-15 10:50:02 +0800

    category heseg ynzalsal

--- src/Pages/Education.js
+++ src/Pages/Education.js
@@ -6,6 +6,13 @@ import BankSystem from "../Components/education/BankSystem";
 import PersonalFinance from "../Components/education/PersonalFinance";
 import Saving from "../Components/education/Saving";
 import Loan from "../Components/education/Loan";
+import BankSystemImg from "../image/banking-system.png"
+import PersonalFinanceImg from "../image/personal-finance.png"
+import SavingImg from "../image/saving.png"
+import LoanImg from "../image/loan.png"
+import InvestmentImg from "../image/investment.png"
+import InsuranceImg from "../image/insurance.png"
+
 function Education() {
   const [link, setLink] = useState();
 
@@ -16,67 +23,78 @@ function Education() {
       <div className={`mt-20 ${link ? `hidden` : `block`}`}>
         <div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-8">
           <div
-            className="mx-auto w-full text-center"
+            className="mx-auto my-4 w-full text-center border  shadow-xl py-8 cursor-pointer transform duration-300 text-primary-dark hover:bg-primary-blue hover:text-primary-white"
             onClick={() => {
               setLink("mongo-ba-bank-sanhuugiin-system");
             }}
           >
-            <div className="h-20 w-20 bg-primary-blue mx-auto">Image 1</div>
-            <div>
+            <div className="h-20 w-20  mx-auto">
+              <img src={BankSystemImg}/>
+            </div>
+            <div className='text-lg  font-semibold'>
               Мөнгө ба Банк <br /> санхүүгийн систем
             </div>
           </div>
 
           <div
-            className="mx-auto w-full text-center"
+            className="mx-auto my-4 w-full text-center border  shadow-xl py-8 cursor-pointer transform duration-300 text-primary-dark hover:bg-primary-blue hover:text-primary-white"
             onClick={() => {
               setLink("huwiin-sanhuugee-udirdah");
             }}
           >
-            <div className="h-20 w-20 bg-primary-blue mx-auto">Image 1</div>
-            <div>
+            <div className="h-20 w-20  mx-auto">
+            <img src={PersonalFinanceImg}/>
+            </div>
+            <div className='text-lg  font-semibold'>
               Хувийн санхүүгээ <br /> удирдах
             </div>
           </div>
 
           <div
-            className="mx-auto w-full text-center"
+            className="mx-auto my-4 w-full text-center border  shadow-xl py-8 cursor-pointer transform duration-300 text-primary-dark hover:bg-primary-blue hover:text-primary-white"
             onClick={() => {
               setLink("hadgalamj");
             }}
           >
-            <div className="h-20 w-20 bg-primary-blue mx-auto">Image 1</div>
-            <div>Хадгаламж</div>
+            <div className="h-20 w-20  mx-auto">
+            <img src={SavingImg}/>
+            </div>
+            <div className='text-lg  font-semibold'>Хадгаламж</div>
           </div>
 
           <div
-            className="mx-auto w-full text-center"
+            className="mx-auto my-4 w-full text-center border  shadow-xl py-8 cursor-pointer transform duration-300 text-primary-dark hover:bg-primary-blue hover:text-primary-white"
             onClick={() => {
               setLink("zeel");
             }}
           >
-            <div className="h-20 w-20 bg-primary-blue mx-auto">Image 1</div>
-            <div>Зээл</div>
+            <div className="h-20 w-20  mx-auto">
+            <img src={LoanImg}/>
+            </div>
+            <div className='text-lg  font-semibold'>Зээл</div>
           </div>
 
           <div
-            className="mx-auto w-full text-center"
+            className="mx-auto my-4 w-full text-center border  shadow-xl py-8 cursor-pointer transform duration-300 text-primary-dark hover:bg-primary-blue hover:text-primary-white"
             onClick={() => {
               setLink("horongo-oruulalt");
             }}
           >
-            <div className="h-20 w-20 bg-primary-blue mx-auto">Image 1</div>
-            <div>Хөрөнгө оруулалт</div>
+            <div className="h-20 w-20  mx-auto"> 
+            <img src={InvestmentImg}/></div>
+            <div className='text-lg  font-semibold'>Хөрөнгө оруулалт</div>
           </div>
 
           <div
-            className="mx-auto w-full text-center"
+            className="mx-auto my-4 w-full text-center border  shadow-xl py-8 cursor-pointer transform duration-300 text-primary-dark hover:bg-primary-blue hover:text-primary-white"
             onClick={() => {
               setLink("daatgal");
             }}
           >
-            <div className="h-20 w-20 bg-primary-blue mx-auto">Image 1</div>
-            <div>Даатгал</div>
+            <div className="h-20 w-20  mx-auto">
+            <img src={InsuranceImg}/>
+            </div>
+            <div className='text-lg  font-semibold'>Даатгал</div>
           </div>
         </div>
       </div>
